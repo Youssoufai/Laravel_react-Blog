59eca04e4936bbe918c3441cc81ef2f77524ef39
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-08-18 20:42:57 +0100

    Second Commit

--- src/Pages/Auth/Register.jsx
+++ src/Pages/Auth/Register.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react"
+
 export default function Register() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: '',
+    });
+    async function handleRegister(e) {
+        e.preventDefault();
+        const res = fetch('api/register')
+        console.log(formData);
+    }
     return (
         <div>
             <h1 className="title">Register your account</h1>
 
-            <form action="">
+            <form onSubmit={handleRegister} className="w-1/2 mx-auto">
                 <div>
-                    <input type="text" name="" placeholder="Name" id="" />
+                    <input type="text" name="" placeholder="Name" value={formData.name} onChange={(e) => setFormData({ ...formData, name: e.target.value })} id="" />
                 </div>
+                <div>
+                    <input type="text" name="" placeholder="Email" onChange={(e) => setFormData({ ...formData, email: e.target.value })} id="" />
+                </div>
+                <div>
+                    <input type="text" name="" onChange={(e) => setFormData({ ...formData, password: e.target.value })} placeholder="Password" id="" />
+                </div>
+                <div>
+                    <input type="text" name="" onChange={(e) => setFormData({ ...formData, password_confirmation: e.target.value })} placeholder="Confirm Password" id="" />
+                </div>
+                <button className="primary-btn">Register</button>
             </form>
         </div>
     )
